35affdff8b51d21d7ba4e74d8063ed96cf39f2be
rejaul <rejaulvoice@gmail.com>
2024-12-08 13:52:57 +0000

    Little bit changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <div className='bg-[#36ab3f] text-white p-10'>
-            <div className='flex justify-between'>
+            <div className='md:flex justify-between'>
                 <div>
                     <h2 className='text-2xl font-bold'>Join Our Newsletter</h2>
                     <p className='text-xs'>And Get 20% OFF your First Order</p>
@@ -26,7 +26,7 @@ const Footer = () => {
             <div className='divide-solid py-8'>
                 <hr />
             </div>
-            <div className='flex justify-between'>
+            <div className='md:flex justify-between'>
                 <div className='flex gap-5'>
                     <p>Shop</p>
                     <p>Reviews</p>
@@ -41,46 +41,6 @@ const Footer = () => {
                     <a href="">Instagram</a>
                 </div>
             </div>
-            {/* <footer className="footer  ">
-            <div className='divide-solid'>
-
-            </div>
-            <nav>
-                <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Company</h6>
-                <Link to="/about" className="link link-hover">About us</Link>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Legal</h6>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
-            </nav>
-            <form>
-                <h6 className="footer-title">Newsletter</h6>
-                <fieldset className="form-control w-80">
-                    <label className="label">
-                        <span className="label-text text-white">Enter your email address</span>
-                    </label>
-                    <div className="join">
-                        <input
-                            type="text"
-                            placeholder="username@site.com"
-                            className="input input-bordered join-item" />
-                        <button className="btn bg-orange-600 join-item">Subscribe</button>
-                    </div>
-                </fieldset>
-            </form>
-        </footer> */}
         </div>
     );
 };
